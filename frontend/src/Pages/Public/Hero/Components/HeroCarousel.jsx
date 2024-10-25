4e788d7b9e9a4e262faacc3d3259ab1e63ee731d
JitenderChoudhary1 <jitender.choudhary@dikonia.in>
2024-10-25 17:54:27 +0530

    fixed sections

--- frontend/src/Pages/Public/Hero/Components/HeroCarousel.jsx
+++ frontend/src/Pages/Public/Hero/Components/HeroCarousel.jsx
@@ -4,9 +4,6 @@ import Img1 from '../../../../assets/Carousel/img1.jpg'
 import Img2 from '../../../../assets/Carousel/img2.jpg'
 import Img3 from '../../../../assets/Carousel/img3.jpg'
 import Img4 from '../../../../assets/Carousel/img4.jpg'
-import carousel1 from '../../../../assets/carousel1.PNG'
-import carousel2 from '../../../../assets/carousel2.PNG'
-import carousel3 from '../../../../assets/carousel3.PNG'
 import carousel4 from '../../../../assets/carousel4.PNG'
 import carousel5 from '../../../../assets/carousel5.PNG'
 import carousel6 from '../../../../assets/carousel6.PNG'
@@ -16,9 +13,16 @@ const ImagesData = [
   Img1,
   Img2,
   Img3,
-  carousel1,
-  carousel2,
-  carousel3,
+  'https://lh3.googleusercontent.com/p/AF1QipO9aAkXCAD7Z-5GW6IJSuKiCkjR6pWrIte-sHnf=s680-w680-h510',
+  'https://www.allduniv.ac.in/public/assets/uploads/media-uploader/nirf-web-new-3a1723533553.jpg',
+  'https://www.allduniv.ac.in/public/assets/uploads/media-uploader/41716277923.jpg',
+  'https://www.allduniv.ac.in/public/assets/uploads/media-uploader/cms-22771706258681.jpg',
+  'https://www.allduniv.ac.in/public/assets/uploads/media-uploader/finalslideimage1702638345.PNG',
+  'https://www.allduniv.ac.in/public/assets/uploads/media-uploader/republic-day-02-20231675247711.jpeg',
+  'https://www.allduniv.ac.in/public/assets/uploads/media-uploader/img-08301668052307.jpg',
+  'https://www.allduniv.ac.in/public/assets/uploads/media-uploader/21627362613.jpg',
+  'https://www.allduniv.ac.in/public/assets/uploads/media-uploader/161627363226.jpg',
+  'https://www.allduniv.ac.in/public/assets/uploads/media-uploader/slider81636461048.jpeg',
   carousel4,
   carousel5,
   carousel6,
